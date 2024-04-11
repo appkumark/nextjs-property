6454a6c9320306a42bfaf787eeb66d9a8464513a
appkumark <appk.kanubuddi@gmail.com>
2024-04-11 14:24:51 +0530

    added env, configure atlasian database, setup mongoose, server api and get data in server component of properties page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import Hero from '@/components/Hero'
+import HomeProperties from '@/components/HomeProperties'
+import InfoBoxes from '@/components/InfoBoxes'
+import connectDB from '@/config/database'
 
 const HomePage = () => {
+  
   return (
-    <div>HomePage</div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
   )
 }
 
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,22 @@
+'use client'
+// import {useRouter, useParams, useSearchParams, usePathname} from 'next/navigation';
+
+const PropertyPage = () => {
+  // const router = useRouter();
+  // const searchParams = useSearchParams()
+  // const {id} = useParams();
+  // const name = searchParams.get('name');
+  // const pathname = usePathname();
+
+  console.log('Hello')
+  return (
+    <div>
+      {/* <button className="bg-blue-500 p-2" onClick={() => router.push('/')}>
+        Go Home {pathname}{' '}
+      </button> */}
+      PropertyPage
+    </div>
+  )
+}
+
+export default PropertyPage
